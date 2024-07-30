5b8644554da1904d47317932e3401a5b24e33b21
SohanGnd <sohan.gonord@hotmail.com>
2024-07-30 17:07:33 +0200

    Add : Header + css + js

---
+++ js/loadHeader.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const headerPlaceholder = document.getElementById("header-placeholder");
+
+  fetch("header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      headerPlaceholder.innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading header:", error));
+});
