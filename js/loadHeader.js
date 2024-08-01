17ccfedc87cadf2face1b11df0f42edc0b194db2
SohanGnd <sohan.gonord@hotmail.com>
2024-08-01 09:39:53 +0200

    Update : Dynamically add header-display on index.html

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -5,6 +5,24 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.text())
     .then((data) => {
       headerPlaceholder.innerHTML = data;
+
+      const currentPage = window.location.pathname;
+
+      if (currentPage.endsWith("index.html")) {
+        const headerOptions = document.querySelector(".header-options");
+        if (headerOptions) {
+          const headerDisplay = document.createElement("div");
+          headerDisplay.id = "header-display";
+          headerDisplay.innerHTML = `
+            <span>Type d'affichage</span>
+            <input type="radio" id="liste" name="type_affichage" checked />
+            <label for="liste" class="options">Liste</label>
+            <input type="radio" id="carte" name="type_affichage" />
+            <label for="carte" class="options">Cartes</label>
+          `;
+          headerOptions.appendChild(headerDisplay);
+        }
+      }
     })
     .catch((error) => console.error("Error loading header:", error));
 });
