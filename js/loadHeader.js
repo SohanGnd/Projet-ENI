611576be122f9999b966d53bd877c2230663fcc6
SohanGnd <sohan.gonord@hotmail.com>
2024-08-02 10:35:51 +0200

    Update: Dynamic Input

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.text())
     .then((data) => {
       headerPlaceholder.innerHTML = data;
-      currentPage = window.location.pathname;
+      const currentPage = window.location.pathname;
 
       if (currentPage.endsWith("index.html")) {
         const headerOptions = document.querySelector(".header-options");
@@ -14,14 +14,70 @@ document.addEventListener("DOMContentLoaded", function () {
           headerDisplay.id = "header-display";
           headerDisplay.innerHTML = `
             <span>Type d'affichage</span>
-            <input type="radio" id="liste" name="type_affichage" checked />
+            <input type="radio" id="liste" name="type_affichage" value="liste" />
             <label for="liste" class="options">Liste</label>
-            <input type="radio" id="carte" name="type_affichage" />
+            <input type="radio" id="carte" name="type_affichage" value="carte" />
             <label for="carte" class="options">Cartes</label>
           `;
           headerOptions.appendChild(headerDisplay);
         }
+
+        applyDisplayPreference();
+
+        document
+          .querySelectorAll('input[name="type_affichage"]')
+          .forEach((input) => {
+            input.addEventListener("change", (event) => {
+              changeDisplay(event.target.value);
+            });
+          });
       }
     })
     .catch((error) => console.error("Error loading header:", error));
 });
+
+function applyDisplayPreference() {
+  const displayPreference = localStorage.getItem("type_affichage") || "liste";
+
+  const apprenantsContainer = document.querySelector(".apprenants-container");
+  if (apprenantsContainer) {
+    apprenantsContainer.classList.add(displayPreference);
+
+    const radioInput = document.querySelector(
+      `input[name="type_affichage"][value="${displayPreference}"]`
+    );
+    if (radioInput) {
+      radioInput.checked = true;
+    }
+
+    const sectionOverflow = document.querySelector(".section_overflow");
+    if (displayPreference === "carte") {
+      sectionOverflow.classList.add("disable-overflow");
+    } else {
+      sectionOverflow.classList.remove("disable-overflow");
+    }
+
+    console.log("Display preference applied:", displayPreference);
+  } else {
+    console.error("Apprenants container not found!");
+  }
+}
+
+function changeDisplay(displayType) {
+  const apprenantsContainer = document.querySelector(".apprenants-container");
+  if (apprenantsContainer) {
+    apprenantsContainer.classList.remove("liste", "carte");
+    apprenantsContainer.classList.add(displayType);
+
+    const sectionOverflow = document.querySelector(".section_overflow");
+    if (displayType === "carte") {
+      sectionOverflow.classList.add("disable-overflow");
+    } else {
+      sectionOverflow.classList.remove("disable-overflow");
+    }
+
+    console.log("Display changed to:", displayType);
+  } else {
+    console.error("Apprenants container not found!");
+  }
+}
